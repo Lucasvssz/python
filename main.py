e34ac53425120aea9d0d74f9ffd1f57f57a36f99
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-01 22:10:35 -0300

    main py

---
+++ main.py
@@ -0,0 +1,50 @@
+# numero_1 = int(input('informe um valor; '))
+# numero_2 = int(input('Informe o segundo valor; '))
+#
+# resultado = numero_1 + numero_2
+#
+# texto= f'o valor {numero_1} mais {numero_2} é igual a {resultado}'
+# print(texto)
+
+# textinho_ = 'Hello, guys'
+#
+# number = len(textinho_)
+# print(number)
+
+# textinho = 'No gays allowed'
+#
+# textinho2 = textinho.replace('No', 'yes')
+#
+# print(textinho2)
+#
+# print(textinho2.startswith('yes'))
+# print(textinho.endswith('ban'))
+# print(textinho.count('allowed'))
+
+# print(chr(77)
+#
+
+# frase = 'Bando de louco'
+#
+# palavras = frase.split()
+#
+# print(' '.join(palavras[::1]))
+
+# email = 'nome@dominio.com'
+#
+# usuario, sep, dominio = email.partion(@)
+
+
+# tabela = str.maketrans('efghijklmnopqrstu', '!@#$%¨&*123478569')
+#
+# print('substituir vogais' . translate(tabela))
+#
+# tabela2 = str.maketrans('!@#$%¨&*123478569', 'efghijklmnopqrstu')
+# print('substituir vogais' . translate(tabela2))
+
+
+#
+# times = ['são lixo', 'porcada', 'corinthians', 'prantos']
+# times.insert(3, 'bragantino')
+#
+# print(times[0])
